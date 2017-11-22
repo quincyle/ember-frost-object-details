09f7ae3770482c138552ec2a6b00dbb01d067d1c
Quincy Le <lexuqing8@gmail.com>
2017-11-22 15:38:21 -0500

    'updateConfigurations'

--- testem.js
+++ testem.js
@@ -1,21 +1,16 @@
 /* eslint-env node */
+var Reporter = require('ember-test-utils/reporter')
+
 module.exports = {
-  'test_page': 'tests/index.html?hidepassed&coverage',
-  'disable_watching': true,
-  'launch_in_ci': [
+  disable_watching: true,
+  framework: 'mocha',
+  launch_in_ci: [
+    'Chrome',
+    'Firefox'
+  ],
+  launch_in_dev: [
     'Chrome'
   ],
-  'launch_in_dev': [
-    'Chrome'
-  ],
-  'launchers': {
-    'PhantomJsVisualAcceptance': {
-      'command': 'phantomjs vendor/phantomjs-launcher.js <url>',
-      'protocol': 'browser'
-    },
-    'SlimerJsVisualAcceptance': {
-      'command': 'slimerjs -jsconsole vendor/phantomjs-launcher.js <url>',
-      'protocol': 'browser'
-    }
-  }
+  reporter: new Reporter(),
+  test_page: 'tests/index.html?hidepassed'
 }
